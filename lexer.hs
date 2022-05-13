f2e00e3f02e97748aaa6082866ae7c48a5034f1b
nicrausaz <n.crausaz99@gmail.com>
2022-05-13 16:27:38 +0200

    Beepbop
    Co-authored-by: Maxime Scharwath <>

---
+++ lexer.hs
@@ -0,0 +1,23 @@
+module Lexer (lexer) where
+
+%wrapper "basic"
+$digit = 0-9
+$alpha = [A-Za-z]
+$alphanum = $alpha $digit
+$ident = $alphanum [_$alphanum]*
+
+tokens :-
+$white+    ;
+let        { \s -> TLet }
+in         { \s -> TIn }
+$digit+    { \s -> TInt (read s) }
+[\=\+\*]   { \s -> TSym (head s) }
+$alpha+    { \s -> TVar s }
+
+{
+type Name = String
+data Token = TLet | TIn | TSym Char | TVar Name | TInt Int deriving (Eq,Show)
+
+lexer = alexScanTokens
+}
+
